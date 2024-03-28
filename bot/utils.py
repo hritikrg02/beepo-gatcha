38d30e147dab2ca3c7aae8f5e0428c3b3d2281eb
Ricky Gupta <hritikrg02@gmail.com>
2024-03-28 16:58:58 -0400

    implemented get_token function

--- bot/utils.py
+++ bot/utils.py
@@ -2,8 +2,10 @@
 # description:  contains utility functions used by the bot
 # author:       Hritik "Ricky" Gupta | hritikrg02@gmail.com
 
-def get_token():
-    pass
+def get_token(token_file):
+    with open(token_file, 'r') as f:
+        token = f.read().rstrip()
+    return token
 
 
 def generate_image():
