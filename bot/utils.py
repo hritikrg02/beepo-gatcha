7de3adf5453ea29f7b674ef333200c55485bc3a2
Ricky Gupta <hritikrg02@gmail.com>
2024-03-28 16:54:35 -0400

    created potential stubs of utility functions

--- bot/utils.py
+++ bot/utils.py
@@ -1,3 +1,10 @@
 # file:         utils.py
 # description:  contains utility functions used by the bot
 # author:       Hritik "Ricky" Gupta | hritikrg02@gmail.com
+
+def get_token():
+    pass
+
+
+def generate_image():
+    pass
