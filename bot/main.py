d145d4914fb9586747457a67cc6d90376c4ad710
Ricky Gupta <hritikrg02@gmail.com>
2024-03-29 00:52:49 -0400

    updated imports and created additional constants for current on_message implementation

--- bot/main.py
+++ bot/main.py
@@ -4,8 +4,9 @@
 
 import discord
 from glob import glob
+from io import BytesIO
 from PIL import Image
-from utils import get_token
+from utils import get_token, generate_image
 
 TOKEN_FILE = "bot/token.txt"
 TOKEN = get_token(TOKEN_FILE)
@@ -13,6 +14,8 @@ TOKEN = get_token(TOKEN_FILE)
 BASE_IMAGES_DIR = "images/base/"
 ACCESSORY_IMAGES_DIR = "images/accessory/"
 EXTENSION = "*.png"
+
+COMPOSITE_FILENAME = "composite.png"
 
 BASE_IMAGES = [Image.open(img) for img in glob(f"{BASE_IMAGES_DIR}{EXTENSION}")]
 ACCESSORY_IMAGES = [Image.open(img) for img in glob(f"{ACCESSORY_IMAGES_DIR}{EXTENSION}")]
@@ -33,8 +36,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    composite = generate_image(BASE_IMAGES, ACCESSORY_IMAGES)
+    buffer = BytesIO()
+    composite.save(buffer, format="PNG")
+    buffer.seek(0)
+    f = discord.File(buffer, filename=COMPOSITE_FILENAME)
+
     if message.content.startswith("$roll"):
-        await message.channel.send(file=discord.File("images/base/clarinet.png"))
+        await message.channel.send(file=f)
 
 
 client.run(TOKEN)
