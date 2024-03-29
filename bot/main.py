713d129ee766030cb342f3027a25af24e2d06142
Ricky Gupta <hritikrg02@gmail.com>
2024-03-28 23:33:21 -0400

    created constants for where images are located

--- bot/main.py
+++ bot/main.py
@@ -3,11 +3,17 @@
 # author:       Hritik "Ricky" Gupta | hritikrg02@gmail.com
 
 import discord
-
+from os import listdir
 from utils import get_token
 
 TOKEN_FILE = "bot/token.txt"
 TOKEN = get_token(TOKEN_FILE)
+
+BASE_IMAGES_DIR = "images/base/"
+ACCESSORY_IMAGES_DIR = "images/accessory/"
+
+BASE_IMAGES = listdir(BASE_IMAGES_DIR)
+ACCESSORY_IMAGES = listdir(ACCESSORY_IMAGES_DIR)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,8 +31,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$roll'):
+        await message.channel.send(file=discord.File("images/base/clarinet.png"))
 
 
 client.run(TOKEN)
